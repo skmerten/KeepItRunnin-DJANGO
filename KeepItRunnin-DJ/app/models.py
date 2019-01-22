ea69100be901609c3765fc9296fd5a04aef58bf8
skmerten <skmerten@gmail.com>
2019-01-21 22:18:49 -0800

    Added Parts Model

--- KeepItRunnin-DJ/app/models.py
+++ KeepItRunnin-DJ/app/models.py
@@ -25,3 +25,24 @@ class Maintenance(models.Model):
     miles = models.IntegerField(max_length=None)
     materials = models.CharField(max_length=250)
     comments = models.CharField(max_length=250)
+
+class Maintenance_History(models.Model):
+    maintenance = models.ForeignKey(Maintenance, on_delete=models.CASCADE)
+    comments = models.CharField(max_length=250)
+    date_completed = models.CharField(max_length=100)
+    next_due_date = models.CharField(max_length=100)
+    next_due_mile = models.IntegerField(max_length=None)
+    completed = models.IntegerField(max_length=None)
+
+class Part(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    maintenance = models.ForeignKey(Maintenance)
+    part_name = models.CharField(max_length=250)
+    date_requested = models.CharField(max_length=50)
+    need_by_date = models.CharField(max_length=50)
+    purchase_location = models.CharField(max_length=250)
+    purchase_price = models.CharField(max_length=50)
+    date_of_purchase = models.CharField(max_length=50)
+    comments = models.CharField(max_length=250)
+    status = models.IntegerField(max_length=None)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # KeepItRunnin-DJANGO
 KeepItRunnin using DJANGO framework to make use of DB models
+python manage.py migrate
+python manage.py sqlmigrate app 0002
