a9a377184b854f22b10c304c0bb07dab859f49be
skmerten <skmerten@gmail.com>
2019-01-21 21:33:03 -0800

    Added Maintenance Model

--- KeepItRunnin-DJ/app/models.py
+++ KeepItRunnin-DJ/app/models.py
@@ -17,3 +17,13 @@ class Vehicle(models.Model):
     phone = models.CharField(max_length=15)
     check_in = models.IntegerField(max_length=None)
     vehicle_creation = models.DateTimeField(auto_now_add=True)
+
+class Maintenance(models.Model):
+    vehicle_id = models.CharField(max_length=100)
+    maintenance_id = models.CharField(max_length=100)
+    item = models.CharField(max_length=100)
+    action = models.CharField(max_length=100)
+    months = models.IntegerField(max_length=None)
+    miles = models.IntegerField(max_length=None)
+    materials = models.CharField(max_length=250)
+    comments = models.CharField(max_length=250)
