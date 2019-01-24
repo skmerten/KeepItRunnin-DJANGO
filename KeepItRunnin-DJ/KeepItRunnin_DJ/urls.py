9f8b10f6c176492f15da35d349ba31c1b4ff6be2
skmerten <skmerten@gmail.com>
2019-01-23 21:24:26 -0800

    FINALLY FIXED URL ISSUE. I Forgot to remove the hard coded URL from the Template!!!!

--- KeepItRunnin-DJ/KeepItRunnin_DJ/urls.py
+++ KeepItRunnin-DJ/KeepItRunnin_DJ/urls.py
@@ -18,18 +18,18 @@ urlpatterns = [
     # /home
     path('', app.views.home, name='home'),
 
-    path('addMaint/', app.views.addMaint, name='addMaint'),
-    path('addParts/', app.views.addParts, name='addParts'),
-    path('addVehicle/', app.views.addVehicle, name='addVehicle'),
-    path('checkIn/', app.views.checkIn, name='checkIn'),
-    path('editMaint/', app.views.editMaint, name='editMaint'),
-    path('histMaint/', app.views.histMaint, name='histMaint'),
-    path('logMaint/', app.views.logMaint, name='logMaint'),
+    path('maintenance/addMaint/', app.views.addMaint, name='addMaint'),
+    path('maintenance/addParts/', app.views.addParts, name='addParts'),
+    path('vehicleProfile/addVehicle/', app.views.addVehicle, name='addVehicle'),
+    path('vehicleProfile/checkIn/', app.views.checkIn, name='checkIn'),
+    path('maintenance/editMaint/', app.views.editMaint, name='editMaint'),
+    path('maintenance/histMaint/', app.views.histMaint, name='histMaint'),
+    path('maintenance/logMaint/', app.views.logMaint, name='logMaint'),
     path('maintenance/', app.views.maintenanceHome, name='maintenanceHome'),
     path('parts/', app.views.partsHome, name='partsHome'),
     path('vehicleProfile/', app.views.vehicleProfile, name='vehicleProfile'),
-    path('viewMaint/', app.views.viewMaint, name='partsHome'),
-    path('viewParts/', app.views.viewPart, name='viewPart'),
+    path('maintenance/viewMaint/', app.views.viewMaint, name='viewMaint'),
+    path('maintenance/viewParts/', app.views.viewPart, name='viewPart'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # path('admin/doc/', include('django.contrib.admindocs.urls')),
