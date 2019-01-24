a297a173f027858d1629861c2b9faa022b0e7c54
skmerten <skmerten@gmail.com>
2019-01-23 21:46:03 -0800

    Re-arranged urls

--- KeepItRunnin-DJ/KeepItRunnin_DJ/urls.py
+++ KeepItRunnin-DJ/KeepItRunnin_DJ/urls.py
@@ -17,19 +17,24 @@ admin.autodiscover()
 urlpatterns = [
     # /home
     path('', app.views.home, name='home'),
-
-    path('maintenance/addMaint/', app.views.addMaint, name='addMaint'),
-    path('maintenance/addParts/', app.views.addParts, name='addParts'),
-    path('vehicleProfile/addVehicle/', app.views.addVehicle, name='addVehicle'),
-    path('vehicleProfile/checkIn/', app.views.checkIn, name='checkIn'),
-    path('maintenance/editMaint/', app.views.editMaint, name='editMaint'),
-    path('maintenance/histMaint/', app.views.histMaint, name='histMaint'),
-    path('maintenance/logMaint/', app.views.logMaint, name='logMaint'),
+    
+    # /maintenance URLs
     path('maintenance/', app.views.maintenanceHome, name='maintenanceHome'),
+    path('maintenance/edit/', app.views.editMaint, name='editMaint'),
+    path('maintenance/history/', app.views.histMaint, name='histMaint'),
+    path('maintenance/log/', app.views.logMaint, name='logMaint'),
+    path('maintenance/add/', app.views.addMaint, name='addMaint'),
+    path('maintenance/view/', app.views.viewMaint, name='viewMaint'),
+
+    # /parts URLs
     path('parts/', app.views.partsHome, name='partsHome'),
-    path('vehicleProfile/', app.views.vehicleProfile, name='vehicleProfile'),
-    path('maintenance/viewMaint/', app.views.viewMaint, name='viewMaint'),
-    path('maintenance/viewParts/', app.views.viewPart, name='viewPart'),
+    path('parts/add/', app.views.addParts, name='addParts'),
+    path('parts/view/', app.views.viewPart, name='viewPart'),
+
+    # /vehicleProfile URLs
+    path('vehicle/', app.views.vehicleProfile, name='vehicleProfile'),
+    path('vehicle/add/', app.views.addVehicle, name='addVehicle'),
+    path('vehicle/checkIn/', app.views.checkIn, name='checkIn'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # path('admin/doc/', include('django.contrib.admindocs.urls')),
