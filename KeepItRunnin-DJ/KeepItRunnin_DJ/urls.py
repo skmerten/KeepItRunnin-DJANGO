240438408a27c984b0a1889a6c05283b700ee22f
skmerten <skmerten@gmail.com>
2019-01-23 21:18:15 -0800

    Updated URLs for 2.1.5

--- KeepItRunnin-DJ/KeepItRunnin_DJ/urls.py
+++ KeepItRunnin-DJ/KeepItRunnin_DJ/urls.py
@@ -3,7 +3,7 @@ Definition of urls for KeepItRunnin_DJ.
 """
 
 from datetime import datetime
-from django.conf.urls import url
+from django.urls import path
 import django.contrib.auth.views
 
 import app.forms
@@ -16,24 +16,24 @@ admin.autodiscover()
 
 urlpatterns = [
     # /home
-    url(r'^$', app.views.home, name='home'),
+    path('', app.views.home, name='home'),
 
-    url(r'^addMaint/', app.views.addMaint, name='addMaint'),
-    url(r'^addParts/', app.views.addParts, name='addParts'),
-    url(r'^addVehicle/', app.views.addVehicle, name='addVehicle'),
-    url(r'^checkIn/', app.views.checkIn, name='checkIn'),
-    url(r'^editMaint/', app.views.editMaint, name='editMaint'),
-    url(r'^histMaint/', app.views.histMaint, name='histMaint'),
-    url(r'^logMaint/', app.views.logMaint, name='logMaint'),
-    url(r'^maintenance/', app.views.maintenanceHome, name='maintenanceHome'),
-    url(r'^parts/', app.views.partsHome, name='partsHome'),
-    url(r'^vehicleProfile/', app.views.vehicleProfile, name='vehicleProfile'),
-    url(r'^viewMaint/', app.views.viewMaint, name='partsHome'),
-    url(r'^viewParts/', app.views.viewPart, name='viewPart'),
+    path('addMaint/', app.views.addMaint, name='addMaint'),
+    path('addParts/', app.views.addParts, name='addParts'),
+    path('addVehicle/', app.views.addVehicle, name='addVehicle'),
+    path('checkIn/', app.views.checkIn, name='checkIn'),
+    path('editMaint/', app.views.editMaint, name='editMaint'),
+    path('histMaint/', app.views.histMaint, name='histMaint'),
+    path('logMaint/', app.views.logMaint, name='logMaint'),
+    path('maintenance/', app.views.maintenanceHome, name='maintenanceHome'),
+    path('parts/', app.views.partsHome, name='partsHome'),
+    path('vehicleProfile/', app.views.vehicleProfile, name='vehicleProfile'),
+    path('viewMaint/', app.views.viewMaint, name='partsHome'),
+    path('viewParts/', app.views.viewPart, name='viewPart'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # path('admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
 ]
