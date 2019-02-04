fcb23529bf3cd49e15fd9e56c6b99fd30f4a5c59
skmerten <skmerten@gmail.com>
2019-02-04 12:09:39 -0800

    Created NewUser Form

--- KeepItRunnin-DJ/app/forms.py
+++ KeepItRunnin-DJ/app/forms.py
@@ -4,6 +4,7 @@ Definition of forms.
 
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from app.models import Vehicle, Maintenance, Maintenance_History, Part, Part_History
 
@@ -18,6 +19,16 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                widget=forms.PasswordInput({
                                    'class': 'form-control',
                                    'placeholder':'Password'}))
+
+class NewUserForm(forms.ModelForm):
+    username = forms.CharField(label='User Name*', max_length=100, required=True, widget=forms.TextInput(attrs={'class' : 'input'}))
+    password = forms.CharField(label='Password*', max_length=100, required=True, widget=forms.PasswordInput(attrs={'class' : 'input'}))
+    email = forms.EmailField(label='Email Address*', max_length=100, required=True, widget=forms.EmailInput(attrs={'class' : 'input'}))
+    first_name = forms.CharField(label='First Name', max_length=100, required=True, widget=forms.TextInput(attrs={'class' : 'input'}))
+    last_name = forms.CharField(label='Last Name', max_length=100, required=True, widget=forms.TextInput(attrs={'class' : 'input'}))
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email', 'first_name', 'last_name', )
 
 # Vehicle Class Forms
 class NewVehicle(forms.ModelForm):
