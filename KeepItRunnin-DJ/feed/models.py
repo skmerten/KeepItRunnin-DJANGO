74533ae8f893c315bec39d6f1c5d957e12207654
skmerten <skmerten@gmail.com>
2019-02-15 10:17:00 -0800

    Updating Models for Feed App and Added Vehicle Record

--- KeepItRunnin-DJ/feed/models.py
+++ KeepItRunnin-DJ/feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from app.models import User
 
-# Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=False)
+    post = models.TextField()
+
+    def __str__(self):
+        return self.user + " " + self.date
